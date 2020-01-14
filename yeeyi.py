f0f6a772f8216d61dce8f2b4553757f9d49da020
Tony <tony.li@redtrain.com.au>
2020-01-14 11:08:22 +1100

    update

--- yeeyi.py
+++ yeeyi.py
@@ -8,6 +8,7 @@ from random import randint
 browser = RoboBrowser(history=True, parser='html.parser')
 
 url = 'your_url'
+log_path = 'path_to_log.log'
 
 login_field = 'username'  # 'email' or 'username'
 username = 'your_username'  # username or email
@@ -16,29 +17,33 @@ password = 'your_password'
 browser.session.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36'
 button = None
 
+
 def login():
     # login
     browser.open('http://www.yeeyi.com/bbs/forum.php')
     form = browser.get_form(id='lsform')
     form['fastloginfield'] = login_field   # username or email
-    form['username'] = username   # username or email
-    form['password'] = password
+    form['username'].value = username.encode('GBK')   # username or email
+    form['password'].value = password
     browser.submit_form(form)
 
-delay = randint(0,600)
-sleep(delay)
+
+#delay = randint(0, 600)
+#sleep(delay)
 browser.open(url)
 button = browser.get_link('提升帖子')
 if not button:
-	login()
-	browser.open(url)
-	button = browser.get_link('提升帖子')
+    login()
+    browser.open(url)
+    button = browser.get_link('提升帖子')
 browser.follow_link(button)
-response = browser.find(class_="alert_right")
-with open('/home/tony/Documents/yeeyi.log','a') as log:
-	log.write('Button clicked, at ' + ctime(mktime(localtime())))
-	if response is None:
-		log.write('No response, please check')
-	else:
-		log.write(response.text)
-#print(response.text)
+response = browser.find(id="messagetext").find("p")
+with open(log_path, 'a') as log:
+    log.write('Button clicked, at ' + ctime(mktime(localtime())) + ' ')
+    if response is None:
+        log.write('No response, please check')
+    else:
+        log.write(response.text)
+# print(response.text)
+
+
